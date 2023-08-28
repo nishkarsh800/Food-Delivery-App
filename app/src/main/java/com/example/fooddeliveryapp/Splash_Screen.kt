3e316e2a8b912db0d893e9e01588da052c65e8e5
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-08-28 14:53:31 +0530

    Implemented the splash screen and start activity

---
+++ app/src/main/java/com/example/fooddeliveryapp/Splash_Screen.kt
@@ -0,0 +1,21 @@
+package com.example.fooddeliveryapp
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this,StartActivity::class.java))
+        },3000)
+
+    }
+}

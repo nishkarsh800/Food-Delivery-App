ff0f8e82ad74d0f8d9ad5daaa8b3a11799aff7f1
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-08-30 20:50:13 +0530

    Minor Fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -37,7 +41,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/fooddeliveryapp/StartActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/StartActivity.kt
@@ -1,11 +1,21 @@
 package com.example.fooddeliveryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fooddeliveryapp.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+
+    private val binding : ActivityStartBinding by lazy{
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+        binding.buttonNext.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
